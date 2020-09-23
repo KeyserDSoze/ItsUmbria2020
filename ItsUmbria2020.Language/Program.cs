aee08d3cb313a04e06b9129e7ac7d336210dc6fd
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2020-09-23 11:42:11 +0200

    Singleton

--- ItsUmbria2020.Language/Program.cs
+++ ItsUmbria2020.Language/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ItsUmbria2020.Language
 {
@@ -7,38 +9,72 @@ namespace ItsUmbria2020.Language
     {
         static List<string> List = new List<string>();
         static List<int> ListOfInt = new List<int>();
-        static void Main(string[] args)
+        //write I
+        //check true
+        //write I
+        //check true
+        //write I
+        //check true
+        //write I
+        //check true
+        //write I
+        //check true
+        //write I
+        //check true
+        //memory x = 2 (1)
+        //if x         (10)
+        //Construction Something
+        //Get Y
+        //Execution WriteLine
+        //Execution Main
+        static void Main(string[] args) //Execution Main
         {
-            Console.WriteLine(new Something(2).Y);
-            int x = 2;
-            if (x < 2)
+            DateTime startTime = DateTime.UtcNow;
+            StringBuilder stringBuilder = new StringBuilder();
+            string a = "A";
+            stringBuilder.Append(a);
+            string b = "B";
+            for (int i = 0; i < 1000000; i++)
             {
+                //a += b;
+                stringBuilder.Append(b);
+            }
+            //Console.WriteLine(stringBuilder);
+            Console.WriteLine(DateTime.UtcNow.Subtract(startTime).TotalMilliseconds);
+            Console.WriteLine(DateTime.UtcNow.Subtract(startTime).TotalSeconds);
+            //Console.WriteLine(new Something(2).Y); //Execution WriteLine //Get Y //Constrution Something
+            //int x = 2;
+            //if (x < 2)
+            //{
 
-            }
-            else if (x > 5)
-            {
+            //}
+            //else if (x > 5)
+            //{
 
-            }
-            else
-            {
-                Console.WriteLine("Hello");
-            }
-            for (int i = 0; i < 10; i++)
-                List.Add((i + 167).ToString());
-            for (int i = 0; i < 10; i++)
-                ListOfInt.Add(i + 167);
-            bool check = true;
-            while (check)
-            {
-                check = !check;
-            }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Hello");
+            //}
+            ////stack overflow here
+            ////while (true)
+            ////    Console.Write("I");
+            //for (int i = 0; i < 10; i++)
+            //    List.Add((i + 167).ToString());
+            //for (int i = 0; i < 10; i++)
+            //    ListOfInt.Add(i + 167);
+            //bool check = true;
+            //while (check)
+            //{
+            //    check = !check;
+            //}
 
-            Console.WriteLine("----------------------------------");
-            for (int i = 0; i < List.Count; i++)
-                Console.WriteLine(List[i]);
-            Console.WriteLine("----------------------------------");
-            foreach (int value in ListOfInt)
-                Console.WriteLine(value);
+            //Console.WriteLine("----------------------------------");
+            //for (int i = 0; i < List.Count; i++)
+            //    Console.WriteLine(List[i]);
+            //Console.WriteLine("----------------------------------");
+            //foreach (int value in ListOfInt)
+            //    Console.WriteLine(value);
         }
     }
     class Something
---
+++ ItsUmbria2020.Singleton/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ItsUmbria2020.Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.BackgroundColor = Styling.Instance().BackgroundColor;
+                Console.ForegroundColor = Styling.Instance().ConsoleColor;
+                
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+    class Styling
+    {
+        public ConsoleColor ConsoleColor { get; }
+        public ConsoleColor BackgroundColor { get; }
+        private static Styling styling;
+        public static Styling Instance()
+        {
+            if (styling == null)
+                styling = new Styling(ConsoleColor.Red, ConsoleColor.Blue);
+            return styling;
+        }
+        private Styling() { }
+        private Styling(ConsoleColor consoleColor, ConsoleColor backgroundColor)
+        {
+            this.ConsoleColor = consoleColor;
+            this.BackgroundColor = backgroundColor;
+        }
+    }
+}
