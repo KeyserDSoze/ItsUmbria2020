2fe3cbc40e5e6b7662c3bc62f78e2b2cd0030846
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2020-09-23 18:07:19 +0200

    Diviso per rendere più chiara la logica

---
+++ ItsUmbria2020.Garbaging/City/Sanitation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItsUmbria2020.Garbaging
+{
+    interface ICamion
+    {
+        void Manage(Junk junk);
+    }
+    class Sanitation
+    {
+        private static readonly Dictionary<string, ICamion> Camions = new Dictionary<string, ICamion>();
+        private static readonly object Semaphore = new object();
+        public static ICamion Instance(string key)
+        {
+            if (!Camions.ContainsKey(key))
+            {
+                lock (Semaphore)
+                {
+                    if (!Camions.ContainsKey(key))
+                    {
+                        if (key == "1")
+                            Camions.Add(key, new GreenCamion());
+                        else if (key == "2")
+                            Camions.Add(key, new Camion());
+                        else
+                            Camions.Add(key, new CleanCamion());
+                    }
+                }
+            }
+            return Camions[key];
+        }
+    }
+    class Camion : ICamion
+    {
+        public void Manage(Junk junk)
+        {
+            Console.WriteLine($"Burn {junk.Name}");
+        }
+    }
+    class GreenCamion : ICamion
+    {
+        public void Manage(Junk junk)
+        {
+            Console.WriteLine($"Recycle {junk.Name}");
+        }
+    }
+    class CleanCamion : ICamion
+    {
+        public void Manage(Junk junk)
+        {
+            Console.WriteLine($"Clean {junk.Name}");
+        }
+    }
+}
---
+++ ItsUmbria2020.Garbaging/Junk/Junk.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItsUmbria2020.Garbaging
+{
+    interface IJunkFactory
+    {
+        Junk GetJunk(string junkType);
+    }
+    class JunkFactory : IJunkFactory
+    {
+        public Junk GetJunk(string junkType)
+        {
+            switch (junkType)
+            {
+                case "1":
+                    return new Plastic();
+                default:
+                case "2":
+                    return new Paper();
+                case "3":
+                    return new Glass();
+            }
+        }
+    }
+    abstract class Junk
+    {
+        public string Name => this.GetType().Name;
+    }
+    class Glass : Junk
+    {
+
+    }
+    class Plastic : Junk
+    {
+
+    }
+    class Paper : Junk
+    {
+
+    }
+}
