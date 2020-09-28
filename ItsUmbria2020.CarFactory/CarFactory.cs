2357e9585b933e0a94f05e7bc30be441b6aae7b0
Alessandro Rapiti <a.rapiti@vetrya.com>
2020-09-28 16:59:01 +0200

    Car Factory

---
+++ ItsUmbria2020.CarFactory/CarFactory.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItsUmbria2020.CarFactory
+{
+    class CarFactory
+    {
+        public Car Create(Owner owner, Data data, string type)
+        {
+            Car car = default;
+            switch (type)
+            {
+                case "1":
+                    car = new Suv();
+                    break;
+                default:
+                case "2":
+                    car = new Coupe();
+                    break;
+            }
+            car.Owner = owner;
+            car.Data = data;
+            return car;
+        }
+    }
+    class Owner
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public override string ToString()
+        {
+            return $"{Name} {Surname}";
+        }
+    }
+    class Data
+    {
+        public string Plate { get; set; }
+        public override string ToString()
+        {
+            return this.Plate;
+        }
+    }
+    abstract class Car
+    {
+        public Owner Owner { get; set; }
+        public Data Data { get; set; }
+        private protected abstract string Addon { get; }
+        public override string ToString()
+        {
+            return $"Abbiamo immatricolato la macchina {this.GetType().Name} {this.Data} {Addon} di {this.Owner}";
+        }
+    }
+    interface IColorable
+    {
+        string Color { get; set; }
+    }
+    interface ICilindrable
+    {
+        int Cilinder { get; set; }
+    }
+    class Suv : Car, ICilindrable
+    {
+        public int Cilinder { get; set; }
+        private protected override string Addon => $"cc{Cilinder}";
+    }
+    class Coupe : Car, IColorable
+    {
+        public string Color { get; set; }
+        private protected override string Addon => $"color: {Color}";
+    }
+}
---
+++ ItsUmbria2020.CarFactory/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ItsUmbria2020.CarFactory
+{
+    class Program
+    {
+        static CarFactory CarFactory = new CarFactory();
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Qui mettete l'inserimento dei campi che servono");
+            //Console.WriteLine("e poi a video deve mostrare l'auto inserita o le auto inserite.");
+            //Console.WriteLine("solo per il SUV vogliamo avere a video anche la cilindrata");
+            //Console.WriteLine("solo per la COUPE vogliamo avere a video anche il colore");
+            //Console.WriteLine("esempio: Abbiamo immatricolato la macchina SUV FF049CA cc2000 di Mario Rossi");
+            //Console.WriteLine("esempio: Abbiamo immatricolato la macchina COUPE rossa FF050CA di Mauro Verdi");
+            //Console.WriteLine("esempio: Abbiamo immatricolato la macchina SW FF050CA di Carlo Carli");
+            string name = RequestValue("nome del proprietario");
+            string surname = RequestValue("cognome del proprietario");
+            string plate = RequestValue("targa");
+            string type = RequestValue("tipologia della macchina: 1) Suv, 2) Coupe");
+            Car car = CarFactory.Create(new Owner { Name = name, Surname = surname }, new Data { Plate = plate }, type);
+            if (car is IColorable)
+            {
+                ((IColorable)car).Color = RequestValue("il colore della macchina");
+            }
+            if (car is ICilindrable)
+            {
+                ((ICilindrable)car).Cilinder = int.Parse(RequestValue("la cilindrata della macchina"));
+            }
+            Console.WriteLine(car);
+        }
+        private static string RequestValue(string what)
+        {
+            Console.WriteLine($"Inserisci {what}");
+            return Console.ReadLine();
+        }
+    }
+}
