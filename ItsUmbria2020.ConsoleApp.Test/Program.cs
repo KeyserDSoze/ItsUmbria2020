7ec3607be6e6b8fab0019f5f53d22d362af1258e
Alessandro Fiani <fiani.alessandro@gmail.com>
2020-09-30 15:50:45 +0200

    New Console app

---
+++ ItsUmbria2020.ConsoleApp.Test/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using ITSGame2020.OnlineGame.Library.Models;
+
+using System;
+using System.Collections.Generic;
+
+namespace Test.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Character> characters = new List<Character>();
+            Warrior warrior = new Warrior();
+            Wizard wizard = new Wizard();
+            Rogue rogue = new Rogue();
+            characters.Add(warrior);
+            characters.Add(wizard);
+            characters.Add(rogue);
+
+            //wizard.ThrowFireBall(warrior);
+
+            foreach (Character character in characters)
+            {
+                Console.WriteLine($"{character.Name} = {character.HealthPoints}");
+                character.Heal(100);
+                Console.WriteLine($"{character.Name} = {character.HealthPoints}");
+
+            }
+        }
+    }
+}
+
