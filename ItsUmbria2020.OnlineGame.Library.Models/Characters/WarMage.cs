0310f198e5263c759b719f9a8c2d6ab2fcc230ce
Alessandro Fiani <fiani.alessandro@gmail.com>
2020-10-05 14:31:34 +0200

    added WarMage override

--- ItsUmbria2020.OnlineGame.Library.Models/Characters/WarMage.cs
+++ ItsUmbria2020.OnlineGame.Library.Models/Characters/WarMage.cs
@@ -6,5 +6,9 @@ namespace ItsUmbria2020.OnlineGame.Library.Models.Characters
 {
     public class WarMage : Wizard
     {
+        public override int BaseDamage => 9;
+        public override int BaseDefense => 8;
+        protected override int BaseHealth => 120;
+        protected override int BaseMana => 110;
     }
 }
