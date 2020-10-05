030376526babe61c97f5cc0b4346b0a828bb7104
Alessandro Fiani <fiani.alessandro@gmail.com>
2020-10-05 14:24:08 +0200

    initial commit

---
+++ ItsUmbria2020.OnlineGame.Library.Models/Characters/WarMage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItsUmbria2020.OnlineGame.Library.Models.Characters
+{
+    public class WarMage : Wizard
+    {
+    }
+}
