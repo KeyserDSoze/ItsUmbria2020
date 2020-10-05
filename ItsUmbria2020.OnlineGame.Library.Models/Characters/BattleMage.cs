c53150a813514f2d76ce361498ef473abc832fd0
Alessandro Fiani <fiani.alessandro@gmail.com>
2020-10-05 14:23:29 +0200

    initial commit

---
+++ ItsUmbria2020.OnlineGame.Library.Models/Characters/BattleMage.cs
@@ -0,0 +1,12 @@
+﻿using ItsUmbria2020.OnlineGame.Library.Models.Interfaces;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItsUmbria2020.OnlineGame.Library.Models.Characters
+{
+    public class BattleMage : IMagicFighter
+    {
+    }
+}
---
+++ ItsUmbria2020.OnlineGame.Library.Models/Interfaces/IMagicFighter.cs
@@ -0,0 +1,6 @@
+﻿namespace ItsUmbria2020.OnlineGame.Library.Models.Interfaces
+{
+    public interface IMagicFighter
+    {
+    }
+}
